e118ae1c7a6ab35447c2068213a34a53a1aa0ebc
rick <rlara@utexas.edu>
2022-08-21 23:11:50 -0500

    fixed repo path

--- docs/make.jl
+++ docs/make.jl
@@ -23,5 +23,5 @@ makedocs(;
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = //github.com/rlarao/AnalyticalEOR.jl"
+    repo = "github.com/rlarao/AnalyticalEOR.jl"
 )
